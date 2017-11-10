82fccef86671a63cb34055ebca425c7ad7eeb993
alejandraHoces <hocesalejandra@gmail.com>
2017-11-09 22:07:19 -0500

    Actualizando archivo js

--- js/app.js
+++ js/app.js
@@ -31,18 +31,16 @@ function addX(event) {
     }
   }
 
-if (count >= 5 && count <= 9) {
+  if (count >= 5 && count <= 9) {
+    toPlay();
+  }
 
-  toPlay()
-}
-
-if (count >=9 && podium === false) {
-  document.getElementById('winner').innerHTML = 'DEAD HEAT';
+  if (count >= 9 && podium === false) {
+    document.getElementById('winner').innerHTML = 'DEAD HEAT';
   }
 };
 
 function toPlay() {
-
   if ((c1.textContent === 'X' && c2.textContent === 'X' && c3.textContent === 'X') ||
  	 (c4.textContent === 'X' && c5.textContent === 'X' && c6.textContent === 'X') ||
  	 (c7.textContent === 'X' && c8.textContent === 'X' && c9.textContent === 'X') ||
@@ -53,9 +51,9 @@ function toPlay() {
       (c3.textContent === 'X' && c5.textContent === 'X' && c7.textContent === 'X')) {
  	 document.getElementById('winner').innerHTML = 'Winner: X ';
  	 count = 10;
-     podium = true;
-     // Declaramos todas las posibles situaciones en la que ganaria O
-   } else if ((c1.textContent === 'O' && c2.textContent === 'O' && c3.textContent === 'O') ||
+    podium = true;
+    // Declaramos todas las posibles situaciones en la que ganaria O
+  } else if ((c1.textContent === 'O' && c2.textContent === 'O' && c3.textContent === 'O') ||
  	 (c4.textContent === 'O' && c5.textContent === 'O' && c6.textContent === 'O') ||
  	 (c7.textContent === 'O' && c8.textContent === 'O' && c9.textContent === 'O') ||
  	 (c1.textContent === 'O' && c4.textContent === 'O' && c7.textContent === 'O') ||
@@ -63,15 +61,15 @@ function toPlay() {
       (c3.textContent === 'O' && c6.textContent === 'O' && c9.textContent === 'O') ||
       (c1.textContent === 'O' && c5.textContent === 'O' && c9.textContent === 'O') ||
       (c.textContent3 === 'O' && c5.textContent === 'O' && c7.textContent === 'O')) {
-     document.getElementById('winner').innerHTML = 'Winner: O ';
-     count = 10;
-     podium = true;
-   } else {
+    document.getElementById('winner').innerHTML = 'Winner: O ';
+    count = 10;
+    podium = true;
+  } else {
 
-   }
- }
+  }
+}
 
 
- function reset() {
-   window.location.reload();
- }
+function reset() {
+  window.location.reload();
+}
